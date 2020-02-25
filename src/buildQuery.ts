a75d17c0dff92adf9c29b126483a08f177fe5c63
Harley Alexander <harley@mayte.io>
2020-02-25 16:58:15 +1100

    ✨ add more query cases

--- src/buildQuery.ts
+++ src/buildQuery.ts
@@ -1,33 +1,71 @@
 import gql from 'graphql-tag';
+import { GET_LIST, GET_ONE, GET_MANY, CREATE, UPDATE, DELETE } from 'ra-core';
 import { buildQueryFactoryOpts } from './types';
 
 export const buildQueryFactory = ({
   queries,
-}: // mutations,
-buildQueryFactoryOpts) => (introspection: any) => (
+  mutations,
+}: buildQueryFactoryOpts) => () => (
   raFetchType: string,
   modelType: string,
   params: Record<string, any>
 ) => {
-  console.log(introspection);
-
   switch (raFetchType) {
-    case 'GET_LIST':
+    case GET_LIST:
       return {
         query: gql(queries[`list${modelType}s`]),
-        variables: { params },
+        variables: {
+          filter: params.filter,
+          limit: params.pagination?.perPage,
+          // we somehow need to override params.pagination.page to return the nextToken instead.
+          // nextToken: params.pagination.page
+        },
         parseResponse: (response: any) => ({
           data: response.data[`list${modelType}s`].items,
           total: null,
         }),
       };
-    case 'GET_ONE':
+    case GET_ONE:
       return {
         query: gql(queries[`get${modelType}`]),
-        variables: { params },
+        variables: params,
         parseResponse: (response: any) => ({
           data: response.data[`get${modelType}`],
         }),
+      };
+    case CREATE:
+      return {
+        query: gql(mutations[`create${modelType}`]),
+        variables: { input: params },
+        parseResponse: (response: any) => ({
+          data: response.data[`create${modelType}`],
+        }),
+      };
+    case UPDATE:
+      return {
+        query: gql(mutations[`update${modelType}`]),
+        variables: { input: { id: params.id, ...params.data } },
+        parseResponse: (response: any) => ({
+          data: response.data[`update${modelType}`],
+        }),
+      };
+    case DELETE:
+      return {
+        query: gql(mutations[`delete${modelType}`]),
+        variables: { input: params },
+        parseResponse: (response: any) => ({
+          data: response.data[`delete${modelType}`],
+        }),
+      };
+    case GET_MANY:
+      return {
+        query: gql(queries[`list${modelType}`]),
+        variables: {
+          limit: params.ids.length,
+          filter: {
+            or: params.ids.map((id: string | number) => ({ id: { eq: id } })),
+          },
+        },
       };
     default:
       return {};
